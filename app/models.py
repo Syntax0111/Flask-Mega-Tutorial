ebc9857fb9e1ba3fd1b8cecd42df017bfaa0850f
Syntax0111 <jrnavarroee@gmail.com>
2019-10-11 14:30:54 -0700

    Chapter 5 completed.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -16,6 +19,14 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -24,3 +35,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
