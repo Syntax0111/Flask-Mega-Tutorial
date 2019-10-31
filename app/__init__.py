35608a63e84006151731ad8f3cfcb8287eeaa13b
Syntax0111 <jrnavarroee@gmail.com>
2019-10-31 11:00:01 -0700

    Chapter 10 completed.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,6 +12,7 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+mail = Mail(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
